bf978165d9e0c3f91e0de68cb30f5f81daa7ea1d
Ian Frost <inf5000@gmail.com>
2015-09-03 17:07:38 -0400

    Modified page-events to design used in pattern lab

--- page-events.php
+++ page-events.php
@@ -6,65 +6,32 @@
 	    <?php endwhile; endif; ?>
 	</div>
 
-	
-	<!--Upcoming Events-->
-	<h2 class="list-title">Upcoming Events</h2>
-	<table caption="Upcoming Events" summary="Frost Finery's Upcoming Events">
-	    <thead>
-	        <tr>
-	            <th id="itemcolumn" scope="col">Event</th>
-	            <th id="col1" scope="col">Location</th>
-	            <th id="col2" scope="col">Date</th>
-	            <th id="col3" scope="col">Time</th>
-	        </tr>
-	    </thead>
-	    <tbody>
-	        <?php
-	        $args = array( 'numberposts' => -1, 'post_type' => 'events', 'orderby' => 'meta_value_num',
-	    'order' => 'ASC' );
-	        $myposts = get_posts( $args );
-	        foreach ( $myposts as $post ) : setup_postdata( $post ); 
-	
-	        $date = get_field('event_start_date');
-	        $time = get_field('event_time');
-	        $image = get_field('event_image');
-	        
-	        if( strtotime($date) >= strtotime('now') ) :
+<!--Begin Upcoming Events-->
+	<h2 class="alpha section-heading">Upcoming Events</h2>
+		<ul class="events-list">
+				 <?php
+		        $args = array( 'numberposts' => -1, 'post_type' => 'events', 'orderby' => 'meta_value_num',
+		    'order' => 'ASC' );
+		        $myposts = get_posts( $args );
+		        foreach ( $myposts as $post ) : setup_postdata( $post );
+		       	 $date = get_field('event_start_date');
+			   	 if( strtotime($date) >= strtotime('now') ) : //If event is later than or equal to the current date
 	        ?>
-	        <tr>
-	                <th scope="row"><a href="<?php the_field('event_url'); ?>"><?php the_title(); ?></a></th>
-	                <td><?php the_field('event_location'); ?></td>
-	                <td>
-	                    <time>
-	                        <?php echo date("M", strtotime($date));?> <?php echo date("d", strtotime($date));?><?php if( get_field('event_end_date') ) {echo "-";$endDate = get_field('event_end_date'); echo date("d", strtotime($endDate));}?> 
-	                        <?php echo date("Y", strtotime($date));?>
-	                    </time>
-	                </td>
-	                <td><?php the_field('event_time'); ?></td>
-			<?php if( get_field('event_image') ) { ?><td><img src=" <?php the_field('event_image'); ?>" /></td><?php } ?>
-	        </tr>
-			<?php 
-				endif; //end future date conditional
-				endforeach; //end loop most ?>
-	    </tbody>
-	</table> 
-	<!--End Upcoming Events-->
-	
-	<!--Past Events-->
-	<h2 class="list-title">Past Events</h2>
-	<table caption="Past Events" summary="Frost Finery's Past Events">
-	    <thead>
-	        <tr>
-	            <th id="itemcolumn" scope="col">Event</th>
-	            <th id="col1" scope="col">Location</th>
-	            <th id="col2" scope="col">Date</th>
-	            <th id="col2" scope="col">Time</th>
-	        </tr>
-	    </thead>
-	    <tbody>
-	        <?php
-	
-	
+					<li>
+						<?php include (TEMPLATEPATH . '/includes/block-event.php'); ?>
+					</li>
+				<?php endif; //end future date conditional
+			endforeach; //end loop most ?>
+		</ul>
+		
+<!--End Upcoming Events-->
+
+<hr />
+	        	
+<!--Past Events-->
+		<h2 class="alpha section-heading">Past Events</h2>
+	<ul class="events-list">
+		<?php
 	        $args = array( 'numberposts' => -1, 'post_type' => 'events', 'orderby' => 'meta_value_num',
 	    'order' => 'DESC' );
 	        $myposts = get_posts( $args );
@@ -73,28 +40,18 @@
 	        $date = get_field('event_start_date');
 	
 	        if( strtotime($date) < strtotime('now') ) : //If event is earlier than the current date
-	
 	        ?>
+
+		<li>
+			<?php include (TEMPLATEPATH . '/includes/block-event.php'); ?>
+		</li>
+		
+		<?php endif; //end previous date conditional
+        endforeach; //end loop most
+        wp_reset_postdata(); 
+        ?>
+	</ul>
 	
-	            <tr>
-	                <th scope="row"><a href="<?php the_field('url'); ?>"><?php the_title(); ?></a></th>
-	                <td><?php the_field('event_location'); ?></td>
-	                <td>
-	                    <time> 
-	                        <?php $date = get_field('event_start_date'); echo date("M", strtotime($date)); echo "&nbsp;".date("d", strtotime($date));?><?php if( get_field('event_end_date') ) { echo "-"; $endDate = get_field('event_end_date'); echo date("d", strtotime($endDate)); }?>
-	                        <?php echo date("Y", strtotime($date));?>
-	                    </time>
-	                </td>
-	                <td><?php the_field('event_time'); ?></td>
-	            </tr>
-	
-	        <?php 
-	        endif; //end future date conditional
-	        endforeach; //end loop most
-	        wp_reset_postdata(); 
-	        ?>
-	    </tbody>
-	</table>
-<!--End Past Speaking Events-->
+<!-- End Past Events-->
 
 <?php get_footer(); ?>
