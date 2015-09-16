1a080d9f83783267a2adaadab6e7a29e14a98f32
Ian Frost <inf5000@gmail.com>
2015-09-16 15:31:48 -0400

    Changed php for collections to include collection and archive

--- page-collections.php
+++ page-collections.php
@@ -1,31 +1,49 @@
 <?php /* Template Name: Collections */ ?>
 <?php get_header(); ?>
-
+<main role="main">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<?php include (TEMPLATEPATH . '/includes/block-hero.php');  ?>
-		<div class="g g-max4 collection-list">
-			<?php 
-				if( have_rows('collection') ):
-				while ( have_rows('collection') ) : the_row(); 
-			?> 
-			<div class="gi">
-				<?php 
-				$link = get_sub_field( "product_link" );
-				if( $link ) : ?>
-				<a href="<?php echo $link; ?>">
-				<?php endif; ?>
-				<?php $image = get_sub_field('product_image'); ?>
-				<img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>">
-				<?php if( $link ) : ?>
-				</a>
-				<?php endif; ?>
-			</div>
-			<?php 
-			    endwhile;
-			    endif;
-			?>
-			
-		</div>
-	<?php endwhile; endif; ?>
-
+	
+<!--Start Collection-->
+	<?php
+		$args = array( 'numberposts' => -1, 'post_type' => 'jewelry', 'orderby' => 'meta_value_num',
+    'order' => 'ASC' );
+        $myposts = get_posts( $args ); ?>
+	<div class="g g-max4 collection-list">
+		 <?php foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+			 <?php if (get_field('section') === "collection" ): ?>
+				<div class="gi">
+					<?php include (TEMPLATEPATH . '/includes/block-jewelry.php'); ?>
+				</div>
+				<?php endif;
+		 endforeach; ?>
+	</div>	
+<!--End Collection-->
+<?php wp_reset_query(); ?>
+	
+	<hr class="hr-short" />
+	
+<!-- Start Archives -->	
+	<?php
+		$title = get_field('archive_title');
+		$excerpt = get_field('archive_excerpt');
+		$override = true;	
+	?>
+	
+	<?php include (TEMPLATEPATH . '/includes/block-hero.php');  
+		$args = array( 'numberposts' => -1, 'post_type' => 'jewelry', 'orderby' => 'meta_value_num',
+    'order' => 'ASC' );
+        $myposts = get_posts( $args ); ?>
+	<div class="g g-max4 collection-list">
+		 <?php foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+			 <?php if (get_field('section') === "archive" ): ?>
+				<div class="gi">
+					<?php include (TEMPLATEPATH . '/includes/block-jewelry.php'); ?>
+				</div>
+			<?php endif;
+		endforeach; ?>
+	</div>
+<!-- End Archives -->
+	<?php endwhile; endif; //end wordpress loop ?>
+</main><!--End role=main-->
 <?php get_footer(); ?>
