adab047685c882b235392b2048b7763578f39f95
Ian Frost <inf5000@gmail.com>
2015-09-23 18:59:01 -0400

    Added block next event

---
+++ includes/block-next-event.php
@@ -0,0 +1,8 @@
+<div class="next-event-list-item">
+	<a href="/events">
+		<h3><?php the_title(); ?></h3>
+		<div class="next-event-info">
+			<?php the_field('event_location'); ?> &mdash; <?php echo date("M", strtotime($date));?> <?php echo date("d", strtotime($date));?><?php if( get_field('event_end_date') ) {echo "-";$endDate = get_field('event_end_date'); echo date("d", strtotime($endDate));}?> <?php echo date("Y", strtotime($date));?>
+		</div>
+	</a>
+</div>
