5f28f95d634a4a51c1d6e8435107b29e4b8ae6b6
Brad Frost <frostbd@gmail.com>
2015-10-23 19:28:19 -0400

    Remove jQuery 1.11

--- functions.php
+++ functions.php
@@ -81,16 +81,16 @@
 
 
 	// Load jQuery
-	if ( !function_exists( 'core_mods' ) ) {
-		function core_mods() {
-			if ( !is_admin() ) {
-				wp_deregister_script( 'jquery' );
-				wp_register_script( 'jquery', ( "http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ), false);
-				wp_enqueue_script( 'jquery' );
-			}
-		}
-		add_action( 'wp_enqueue_scripts', 'core_mods' );
-	}
+	// if ( !function_exists( 'core_mods' ) ) {
+	// 	function core_mods() {
+	// 		if ( !is_admin() ) {
+	// 			wp_deregister_script( 'jquery' );
+	// 			wp_register_script( 'jquery', ( "http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ), false);
+	// 			wp_enqueue_script( 'jquery' );
+	// 		}
+	// 	}
+	// 	add_action( 'wp_enqueue_scripts', 'core_mods' );
+	// }
 
 	// Clean up the <head>, if you so desire.
 	//	function removeHeadLinks() {
