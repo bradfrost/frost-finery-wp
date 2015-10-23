c1c70d79f1f9ef81918b5f4d56a00a6ce06fc791
Ian Frost <inf5000@gmail.com>
2015-10-23 15:21:55 -0400

    Added Tout Size

--- includes/block-tout.php
+++ includes/block-tout.php
@@ -2,7 +2,7 @@
 	<a href="<?php the_sub_field('tout_link'); ?>">
 		<h4 class="b-eyebrow"><?php the_sub_field('tout_eyebrow'); ?></h4>
 		<h3 class="b-title"><?php the_sub_field('tout_title'); ?></h3>
-		<?php $image = get_sub_field('tout_img'); ?>
-		<img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>">
+		<?php $image = get_sub_field('tout_img'); ?> 
+		<img src="<?php echo $image['sizes']['tout-size']; ?>" alt="<?php echo $image['alt'] ?>">
 	</a>
 </div>
