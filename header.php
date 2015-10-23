401282bfc9b6370fee90b69e87591285c438ce07
Brad Frost <frostbd@gmail.com>
2015-10-23 11:11:09 -0400

    SVG logo
    Inlined logo and added hover state
    
    Close #5

--- header.php
+++ header.php
@@ -6,12 +6,12 @@
 <body <?php body_class(); ?>>
 	<header class="header" role="banner">
 		<div class="lc">
-			<h3 class="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="<?php bloginfo( 'name' ); ?>" /></a></h3>
+			<h3 class="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php include (TEMPLATEPATH . '/includes/logo.php'); ?></a></h3>
 			<a href="#nav" class="nav-toggle">Menu</a>
 			<nav id="nav" class="nav" role="navigation">
 				<?php wp_nav_menu( array('menu' => 'primary') ); ?>
 			</nav>
 		</div>
 	</header>
-	
+
 	<main role="main">
