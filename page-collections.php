10aa7e00a9c46f36fb09f1aafed96ece1a1c0ea3
Ian Frost <inf5000@gmail.com>
2017-10-24 12:50:06 -0400

    Added organic, minimalist, pittsburgh local, home. Need to remove collections/accessories once items are put into place

--- page-collections.php
+++ page-collections.php
@@ -1,15 +1,15 @@
 <?php /* Template Name: Collections */ ?>
 <?php get_header(); ?>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
+
 <!--Start Seasonal Collection-->
 <?php
 		$title = get_field('seasonal_title');
 		$excerpt = get_field('seasonal_excerpt');
-		$override = true;	
+		$override = true;
 ?>
 	<?php
-		include (TEMPLATEPATH . '/includes/block-hero.php'); 
+		include (TEMPLATEPATH . '/includes/block-hero.php');
 		$args = array( 'numberposts' => -1, 'post_type' => 'jewelry', 'orderby' => 'menu_order',
     'order' => 'ASC' );
         $myposts = get_posts( $args ); ?>
@@ -21,18 +21,18 @@
 				</div>
 				<?php endif;
 		 endforeach; ?>
-	</div>	
+	</div>
 <!--End Seasonal Collection-->
 	<?php wp_reset_query(); ?>
-	
+
 <hr class="hr-short" />
-		
-		
+
+
 <!--Start Collection-->
 	<?php
 		$title = get_field('collection_title');
 		$excerpt = get_field('collection_excerpt');
-		$override = true;	
+		$override = true;
 	?>
 	<?php
 		include (TEMPLATEPATH . '/includes/block-hero.php');
@@ -47,46 +47,150 @@
 				</div>
 				<?php endif;
 		 endforeach; ?>
-	</div>	
+	</div>
 <!--End Collection-->
 <?php wp_reset_query(); ?>
-	
+
 	<hr class="hr-short" />
-	
-<!-- Start Accessories -->
+
+	<!-- Start Home -->
+	<?php
+			$title = get_field('accessories_title');
+			$excerpt = get_field('accessories_excerpt');
+			$override = true;
+		?>
+
+		<?php include (TEMPLATEPATH . '/includes/block-hero.php');
+			$args = array( 'numberposts' => -1, 'post_type' => 'jewelry', 'orderby' => 'menu_order',
+	    'order' => 'ASC' );
+	        $myposts = get_posts( $args ); ?>
+		<div class="g g-max4 collection-list">
+			 <?php foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+				 <?php if (get_field('section') === "accessories" ): ?>
+					<div class="gi">
+						<?php include (TEMPLATEPATH . '/includes/block-jewelry.php'); ?>
+					</div>
+				<?php endif;
+			endforeach; ?>
+		</div>
+	<!-- End Home -->
+
+	<?php wp_reset_query(); ?>
+
+		<hr class="hr-short" />
+
+<!-- Start organic-->
 <?php
-		$title = get_field('accessories_title');
-		$excerpt = get_field('accessories_excerpt');
-		$override = true;	
+		$title = get_field('organic_title');
+		$excerpt = get_field('organic_excerpt');
+		$override = true;
 	?>
-	
-	<?php include (TEMPLATEPATH . '/includes/block-hero.php');  
+
+	<?php include (TEMPLATEPATH . '/includes/block-hero.php');
 		$args = array( 'numberposts' => -1, 'post_type' => 'jewelry', 'orderby' => 'menu_order',
     'order' => 'ASC' );
         $myposts = get_posts( $args ); ?>
 	<div class="g g-max4 collection-list">
 		 <?php foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-			 <?php if (get_field('section') === "accessories" ): ?>
+			 <?php if (get_field('section') === "organic" ): ?>
 				<div class="gi">
 					<?php include (TEMPLATEPATH . '/includes/block-jewelry.php'); ?>
 				</div>
 			<?php endif;
 		endforeach; ?>
 	</div>
-<!-- End Accessories -->
+<!-- End Organic -->
 
 <?php wp_reset_query(); ?>
-	
-	<hr class="hr-short" />	
-	
-<!-- Start Archivess -->	
+
+	<hr class="hr-short" />
+
+<!-- Start Minimalist-->
+<?php
+		$title = get_field('minimalist_title');
+		$excerpt = get_field('minimalist_excerpt');
+		$override = true;
+	?>
+
+	<?php include (TEMPLATEPATH . '/includes/block-hero.php');
+		$args = array( 'numberposts' => -1, 'post_type' => 'jewelry', 'orderby' => 'menu_order',
+    'order' => 'ASC' );
+        $myposts = get_posts( $args ); ?>
+	<div class="g g-max4 collection-list">
+		 <?php foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+			 <?php if (get_field('section') === "minimalist" ): ?>
+				<div class="gi">
+					<?php include (TEMPLATEPATH . '/includes/block-jewelry.php'); ?>
+				</div>
+			<?php endif;
+		endforeach; ?>
+	</div>
+<!-- End Minimalist -->
+
+<?php wp_reset_query(); ?>
+
+	<hr class="hr-short" />
+
+<!-- Start Pittsburgh Local-->
+<?php
+		$title = get_field('pittsburgh_local_title');
+		$excerpt = get_field('pittsburgh_local_excerpt');
+		$override = true;
+	?>
+
+	<?php include (TEMPLATEPATH . '/includes/block-hero.php');
+		$args = array( 'numberposts' => -1, 'post_type' => 'jewelry', 'orderby' => 'menu_order',
+    'order' => 'ASC' );
+        $myposts = get_posts( $args ); ?>
+	<div class="g g-max4 collection-list">
+		 <?php foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+			 <?php if (get_field('section') === "pittsburgh-local" ): ?>
+				<div class="gi">
+					<?php include (TEMPLATEPATH . '/includes/block-jewelry.php'); ?>
+				</div>
+			<?php endif;
+		endforeach; ?>
+	</div>
+<!-- End Pittsburgh Local -->
+
+<?php wp_reset_query(); ?>
+
+	<hr class="hr-short" />
+
+<!-- Start Home -->
+<?php
+		$title = get_field('home_title');
+		$excerpt = get_field('home_excerpt');
+		$override = true;
+	?>
+
+	<?php include (TEMPLATEPATH . '/includes/block-hero.php');
+		$args = array( 'numberposts' => -1, 'post_type' => 'jewelry', 'orderby' => 'menu_order',
+    'order' => 'ASC' );
+        $myposts = get_posts( $args ); ?>
+	<div class="g g-max4 collection-list">
+		 <?php foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+			 <?php if (get_field('section') === "home" ): ?>
+				<div class="gi">
+					<?php include (TEMPLATEPATH . '/includes/block-jewelry.php'); ?>
+				</div>
+			<?php endif;
+		endforeach; ?>
+	</div>
+<!-- End Home -->
+
+<?php wp_reset_query(); ?>
+
+	<hr class="hr-short" />
+
+<!-- Start Archivess -->
 	<?php
 		$title = get_field('archive_title');
 		$excerpt = get_field('archive_excerpt');
-		$override = true;	
+		$override = true;
 	?>
-	
-	<?php include (TEMPLATEPATH . '/includes/block-hero.php');  
+
+	<?php include (TEMPLATEPATH . '/includes/block-hero.php');
 		$args = array( 'numberposts' => -1, 'post_type' => 'jewelry', 'orderby' => 'menu_order',
     'order' => 'ASC' );
         $myposts = get_posts( $args ); ?>
