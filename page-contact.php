9bb0c1bf7635798298a961105704bf8ff4e21685
Brad Frost <brad@bradfrost.com>
2019-03-02 19:47:32 -0500

    Remove contact form

--- page-contact.php
+++ page-contact.php
@@ -16,16 +16,7 @@
 				<a href="#custom-order" class="btn">Request A Custom Order</a>
 			</div>
 		</div>
-		<hr class="hr-short" id="custom-order" />
-		<?php
-		$title = get_field('contact_title');
-		$excerpt = get_field('contact_excerpt');
-		$override = true;
-	?>
-		<?php include (TEMPLATEPATH . '/includes/block-hero.php');  ?>
-		<div class="lc-single">
-			<?php echo do_shortcode( '[contact-form-7 id="340" title="Request A Custom Order"]' ); ?>
-		</div>
+		
 	<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
